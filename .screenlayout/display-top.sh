4f337f124930e53fe130cc2bbea2bce79d176e01
J. Eduardo <j.eduardo@gmail.com>
2018-02-15 22:10:07 +0100

    i3: Added current configuration for i3.

---
+++ .screenlayout/display-top.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --off --output eDP-1 --primary --mode 1440x900 --pos 272x1080 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --mode 1920x1080 --rate 59.94 --pos 0x0 --rotate normal --output DP-1 --off
