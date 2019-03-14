4b296687530186c8ca12eb3e6cf59d94226a28b2
J. Eduardo <j.eduardo@gmail.com>
2019-03-14 19:17:27 +0100

    xorg: add configuration for multiple displays with an NVIDIA card

---
+++ .screenlayout/display-nvidia.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL3 --off --output VIRTUAL2 --off --output VIRTUAL1 --off --output VIRTUAL7 --off --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal --output VIRTUAL5 --mode VIRTUAL5.451-1920x1080 --pos 0x0 --rotate normal --output VIRTUAL4 --off --output VIRTUAL8 --off --output VIRTUAL6 --off
