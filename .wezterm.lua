63f1d6bd655e2b7a21cd0ec3201d10d14c10bc9a
J. Eduardo <j.eduardo@gmail.com>
2023-05-07 15:07:54 +0200

    wezterm: added theme change when desktop theme change

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,6 +8,21 @@ wezterm.on('gui-startup', function(cmd)
   --window:gui_window():maximize()
 end)
 
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function scheme_for_appearance(apperance)
+  if apperance:find 'Dark' then
+    return 'Dracula'
+  else
+    return 'Builtin Solarized Light'
+  end
+end
+
 return {
   window_close_confirmation = 'NeverPrompt',
   font = wezterm.font_with_fallback(
@@ -24,7 +39,7 @@ return {
   --cell_width = 1.0,
   --line_height = 1.0
   --freetype_render_target = 'Light',
-  
+
   hide_tab_bar_if_only_one_tab = true,
   hide_mouse_cursor_when_typing = false,
 
@@ -36,9 +51,11 @@ return {
     { key = 't', mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
     { key = 't', mods = 'SUPER', action = act.SpawnTab 'CurrentPaneDomain' },
   },
+
+  color_scheme = scheme_for_appearance(get_appearance())
+
   --color_scheme = 'Tangoesque (terminal.sexy)',
   --color_scheme = 'Solarized Dark (base16)',
-  color_scheme = 'Dracula',
   --color_scheme = 'WildCherry',
   --color_scheme = 'Terminix Dark (Gogh)',
   --color_scheme = 'theme2 (terminal.sexy)',
@@ -46,28 +63,28 @@ return {
   --color_scheme = 'Solarized Dark Higher Contrast',
   --color_scheme = 'Synth Midnight Terminal Dark (base16)',
   --color_scheme = 'Papercolor Dark (Gogh)',
-  colors = {
-    cursor_bg = '#c0c0c0',
-    foreground = '#c0c0c0',
-    ansi = {
-      '#2e3436', --black
-      '#cc0000', -- red
-      '#4e9a06', -- green
-      '#c4a000', -- yellow
-      '#3465a4', -- blue
-      '#75507b', -- magenta
-      '#06989a', -- cyan
-      '#d3d7cf', -- white
-    },
-    brights = {
-      '#555753', -- black
-      '#ef2929', -- red
-      '#8ae234', -- green
-      '#fce94f', -- yellow
-      '#729fcf', -- blue
-      '#ad7fa8', -- magenta
-      '#34e2e2', -- cyan
-      '#eeeeec', -- white
-    },
-  },
+  --colors = {
+  --  cursor_bg = '#c0c0c0',
+  --  foreground = '#c0c0c0',
+  --  ansi = {
+  --    '#2e3436', --black
+  --    '#cc0000', -- red
+  --    '#4e9a06', -- green
+  --    '#c4a000', -- yellow
+  --    '#3465a4', -- blue
+  --    '#75507b', -- magenta
+  --    '#06989a', -- cyan
+  --    '#d3d7cf', -- white
+  --  },
+  --  brights = {
+  --    '#555753', -- black
+  --    '#ef2929', -- red
+  --    '#8ae234', -- green
+  --    '#fce94f', -- yellow
+  --    '#729fcf', -- blue
+  --    '#ad7fa8', -- magenta
+  --    '#34e2e2', -- cyan
+  --    '#eeeeec', -- white
+  --  },
+  --},
 }
