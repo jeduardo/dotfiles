99c8a74d40792dc72ddb9c813dbbba401b606431
J. Eduardo <j.eduardo@gmail.com>
2023-10-14 15:30:55 +0200

    wezterm: window padding

--- .wezterm.lua
+++ .wezterm.lua
@@ -71,6 +71,12 @@ return {
     border_bottom_color = 'black',
     border_top_color = 'black',
   },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   font = wezterm.font_with_fallback(
     {
